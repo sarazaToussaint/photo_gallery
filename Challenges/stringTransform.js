e63cbf7a4e6a5369e1826f7c6f6ee5cdbcc11042
sarazaToussaint <tmsaraza@gmail.com>
2024-06-06 13:39:44 +0300

    Refine codes in this file

--- Challenges/stringTransform.js
+++ Challenges/stringTransform.js
@@ -1,22 +1,16 @@
-function transformString(input) {
+const transformString = (input) => {
   const { length } = input;
 
-  // If the length is divisible by both 3 and 5, it must be divisible by 15.
   if (length % 15 === 0) {
-    // Reverse the string first
     input = input.split('').reverse().join('');
-    // Replace each character with its ASCII code
     input = input.split('').map((char) => char.charCodeAt(0)).join(' ');
   } else if (length % 5 === 0) {
-    // Replace each character with its ASCII code
     input = input.split('').map((char) => char.charCodeAt(0)).join(' ');
   } else if (length % 3 === 0) {
-    // Reverse the string
     input = input.split('').reverse().join('');
   }
-
   return input;
-}
+};
 
 // Test cases
 console.log(transformString('Hamburger')); // Output: "regrubmaH"
